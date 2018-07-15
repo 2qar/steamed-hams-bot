de28f3f4b1ea6dad3873faf6cd1342e3ab0397c3
BigHeadGeorge <tucker1014@hotmail.com>
2018-07-15 01:10:03 -0700

    Update Ref.java

--- src/main/java/com/mycompany/steamedhamsbot/Ref.java
+++ src/main/java/com/mycompany/steamedhamsbot/Ref.java
@@ -2,7 +2,7 @@ package com.mycompany.steamedhamsbot;
 
 public class Ref 
 {
-    public static final String TOKEN = "NDM3NzkzNjM5MjQzMTg2MTg5.Db7Uvg.EzPsw4GMTNodRt5eTVHMAUudjiI";
+    public static final String TOKEN = ""; // insert your own token here
     public static final String PREFIX = "<>";
     public static final String INVITE_LINK = "https://discordapp.com/oauth2/authorize?client_id=437793639243186189&scope=bot&permissions=101440";
 }
